8228f3c6da160ccd11463ed82851ec5c6bec9745
brookebachman00 <bbachman1994@gmail.com>
2020-10-28 14:44:42 -0600

    index.html

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 const nav = document.getElementById('page-header')
 nav.addEventListener("click", function(event){
-    console.log(event.target)
-    let currentId = event.target.id
+    // console.log(event.target.id)
+    // let currentId = event.target.id
 
-    if (currentId === "about"){
-        let el = document.getElementById("about")
-        el.scrollIntoView(true);
-    } else if (currentId === "contact"){
-        let el = document.getElementById("contact")
-        el.scrollIntoView(true);
-    } else if (currentId === "socials"){
-        let el = document.getElementById("socials")
-        el.scrollIntoView(true);
-    }
-    
+    // if (currentId === "about"){
+    //     let el = document.getElementById("about")
+    //     el.scrollIntoView(true);
+    // } else if (currentId === "contact"){
+    //     let el = document.getElementById("contact")
+    //     el.scrollIntoView(true);
+    // } else if (currentId === "socials"){
+    //     let el = document.getElementById("socials")
+    //     el.scrollIntoView(true);
+    // }
+    document.getElementById("contact").scrollIntoView({behavior: 'smooth'});
 })
