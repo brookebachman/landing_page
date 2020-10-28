dfe01dbd73ef4d47941ce417e34702ec9f9a8266
brookebachman00 <bbachman1994@gmail.com>
2020-10-28 14:50:27 -0600

    finally got the code to work scrolling with javascriptgit push

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 const nav = document.getElementById('page-header')
 nav.addEventListener("click", function(event){
-    // console.log(event.target.id)
-    // let currentId = event.target.id
-
-    // if (currentId === "about"){
-    //     let el = document.getElementById("about")
-    //     el.scrollIntoView(true);
-    // } else if (currentId === "contact"){
-    //     let el = document.getElementById("contact")
-    //     el.scrollIntoView(true);
-    // } else if (currentId === "socials"){
-    //     let el = document.getElementById("socials")
-    //     el.scrollIntoView(true);
-    // }
-    document.getElementById("contact").scrollIntoView({behavior: 'smooth'});
+    console.log(event.target.id)
+    let currentId = event.target.id
+    if (currentId === "about"){
+        let el = document.getElementById("about1")
+        el.scrollIntoView(true);
+    } else if (currentId === "contact"){
+        let el = document.getElementById("contact1")
+        el.scrollIntoView(true);
+    } else if (currentId === "socials"){
+        let el = document.getElementById("socials1")
+        el.scrollIntoView(true);
+     }
+    //document.getElementById("contact").scrollIntoView({behavior: 'smooth'});
 })
