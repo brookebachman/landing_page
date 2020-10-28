9d540d95a13eb01f4ec842ea4bba20d2538fdda8
brookebachman00 <bbachman1994@gmail.com>
2020-10-28 13:01:33 -0600

    Added the folders

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// function smoothScroll(target2, duration){
+//     console.log(target2);
+//     const target = document.querySelector(target2);
+//     const targetPosition = target.getBoundingClientRectangle().top;
+//     const startPosition = window.pageYOffset;
+//     const distance = Math.abs(targetPosition) - Math.abs(startPosition);
+//     console.log(target);
+//     const startTime = null;
+// }
+
+// smoothScroll('.section2', 1000);
