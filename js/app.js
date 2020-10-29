5c3e066f9072a4f6357a7377b7a7bdeb26cbeaff
brookebachman00 <bbachman1994@gmail.com>
2020-10-29 10:55:16 -0600

    Working on making social media pretty

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ nav.addEventListener("click", function(event){
     //document.getElementById("contact").scrollIntoView({behavior: 'smooth'});
 })
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -38,17 +38,17 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].class = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += "active";
 }
