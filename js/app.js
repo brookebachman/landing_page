6636afd7661c76fc4f3e071f59683d5917a91021
brookebachman00 <bbachman1994@gmail.com>
2020-11-05 12:14:24 -0700

    Still not working

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
   addLi();
   const nav = document.getElementById('page-header');
 
-  //I put an event listener on the navigation container, this is easier and more dry than creating a new event listener for each button
-//Then I grab the link elements on the page
-//Next I look to see what link matches what was clicked
-//Once I find which link was clicked then I want to take the section that matches that link and scroll it to the top view of the page
-//I used setTimeout to allow me to essentially unclick the link after 2 seconds so that the user can click a new link and only the newest link they clicked is highlighted
+    //I put an event listener on the navigation container, this is easier and more dry than creating a new event listener for each button
+  //Then I grab the link elements on the page
+  //Next I look to see what link matches what was clicked
+  //Once I find which link was clicked then I want to take the section that matches that link and scroll it to the top view of the page
+  //I used setTimeout to allow me to essentially unclick the link after 2 seconds so that the user can click a new link and only the newest link they clicked is highlighted
   function addLi(){
     console.log("i am running")
     let ul = document.getElementById("links");
@@ -19,26 +19,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
   nav.addEventListener('click', function (event) {
     console.log(event.target);
     let currentId = event.target.id;
-    if (currentId === 'About') {
-      let currentSection = about;
+    if (currentId === '0') {
+      let currentSection = document.getElementById("About");
       currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
       about.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
        //currentSection.style.cssText = 'border: 15px solid lightgray';
       setTimeout(unClick(about), 2000);
-    } else if (currentId === 'contact') {
-      let currentSection = contact;
+    } else if (currentId === '1') {
+      let currentSection = document.getElementById("contact");
       currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
       //contact.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
       // currentSection.style.cssText = 'border: 15px solid lightgray';
       setTimeout(unClick(contact), 2000);
-    } else if (currentId === 'socials') {
-      let currentSection = socials;
+    } else if (currentId === '2') {
+      let currentSection = document.getElementById("socials");
       currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
       socials.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
       //currentSection.style.cssText = 'border: 15px solid lightgray';
       setTimeout(unClick(socials), 2000);
-    } else if (currentId === 'extra') {
-      let currentSection = extra;
+    } else if (currentId === '3') {
+      let currentSection = document.getElementById("bonus");
       currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
       extra.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
       //currentSection.style.cssText = 'border: 15px solid lightgray';
@@ -54,20 +54,12 @@ const navbarMenu = document.getElementById("links");
 const sections = document.querySelectorAll("section");
 //build a nav
 function createNav(){
-  // for (let item of sections){
-  //   let section = document.createElement("li");
-  //   section.className = "menu__link";
-  //   section.innerText = item.id;
-  //   navbarMenu.appendChild(section);
-
     for (let i = 0; i < 4; i++){
       let section = document.createElement("li");
       section.className = "menu__link";
       section.id = i;
       navbarMenu.appendChild(section);
-      
-    
-  };  
+    };  
 };
 createNav();
 
@@ -79,6 +71,3 @@ about.innerText = "About";
 contact.innerText = "Contact";
 socials.innerText = "Socials";
 extra.innerText = "Bonus Content";
-
-
-// Thumbnail image controls
