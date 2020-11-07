430a25b8fe8426ce7f8c6f263b85b745d5902cb4
brookebachman00 <bbachman1994@gmail.com>
2020-11-07 15:55:00 -0700

    Final version

--- js/app.js
+++ js/app.js
@@ -1,51 +1,55 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  addLi();
-  const nav = document.getElementById('page-header');
+  console.log('DOM fully loaded and parsed');
+ 
+  
 
     //I put an event listener on the navigation container, this is easier and more dry than creating a new event listener for each button
   //Then I grab the link elements on the page
   //Next I look to see what link matches what was clicked
   //Once I find which link was clicked then I want to take the section that matches that link and scroll it to the top view of the page
-  //I used setTimeout to allow me to essentially unclick the link after 2 seconds so that the user can click a new link and only the newest link they clicked is highlighted
-  function addLi(){
-    console.log("i am running")
-    let ul = document.getElementById("links");
-    let i = 0;
-    while (i <= 4){
-      let li = document.createElement('li');
-      ul.appendChild(li);
-    }
+ 
+addLi();
+function addLi(){
+  console.log("i am running")
+  let ul = document.getElementById("links");
+  let i = 0;
+  while (i <= 4){
+    let li = document.createElement('li');
+    ul.appendChild(li);
   }
-  nav.addEventListener('click', function (event) {
-    console.log(event.target);
-    let currentId = event.target.id;
-    if (currentId === '0') {
-      let currentSection = document.getElementById("about");
-      currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-      about.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
-       //currentSection.style.cssText = 'border: 15px solid lightgray';
-      setTimeout(unClick(about), 2000);
-    } else if (currentId === '1') {
-      let currentSection = document.getElementById("contact");
-      currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-      //contact.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
-      // currentSection.style.cssText = 'border: 15px solid lightgray';
-      setTimeout(unClick(contact), 2000);
-    } else if (currentId === '2') {
-      let currentSection = document.getElementById("socials");
-      currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-      socials.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
-      //currentSection.style.cssText = 'border: 15px solid lightgray';
-      setTimeout(unClick(socials), 2000);
-    } else if (currentId === '3') {
-      let currentSection = document.getElementById("bonus");
-      currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-      extra.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
-      //currentSection.style.cssText = 'border: 15px solid lightgray';
-      setTimeout(unClick(extra), 2000);
-    }
-  });
+}
+  
 })
+const nav = document.getElementById('page-header');
+nav.addEventListener('click', function (event) {
+  console.log(event.target);
+  let currentId = event.target.id;
+  if (currentId === '0') {
+    let currentSection = document.getElementById("about");
+    currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    about.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
+     //currentSection.style.cssText = 'border: 15px solid lightgray';
+    setTimeout(unClick(about), 2000);
+  } else if (currentId === '1') {
+    let currentSection = document.getElementById("contact");
+    currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    //contact.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
+    // currentSection.style.cssText = 'border: 15px solid lightgray';
+    setTimeout(unClick(contact), 2000);
+  } else if (currentId === '2') {
+    let currentSection = document.getElementById("socials");
+    currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    socials.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
+    //currentSection.style.cssText = 'border: 15px solid lightgray';
+    setTimeout(unClick(socials), 2000);
+  } else if (currentId === '3') {
+    let currentSection = document.getElementById("bonus");
+    currentSection.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+    extra.style.cssText = 'background-color: rgb(244, 226, 229, .8);';
+    //currentSection.style.cssText = 'border: 15px solid lightgray';
+    setTimeout(unClick(extra), 2000);
+  }
+});
 function unClick(current) {
 	current.style.cssText = 'background-color: black';
 }
@@ -56,11 +60,11 @@ const sections = document.querySelectorAll("section");
 function createNav(){
     for (let i = 0; i < 4; i++){
       let section = document.createElement("li");
-      section.className = "menu__link";
       section.id = i;
       navbarMenu.appendChild(section);
     };  
 };
+
 createNav();
 
 const about = document.getElementById('0');
